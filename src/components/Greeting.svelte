aa32a5e0ec41d1b10037e27838f8bf307c6429c9
Eduardo Maximiliano <maximilianocm06@gmail.com>
2024-01-31 00:04:32 -0600

    Cambia el saludo segun la hora

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,14 @@
+<script>
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting = "";
+  if (currentHour < 12) {
+    greeting = "Buenos días";
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes";
+  } else {
+    greeting = "Buenas noches";
+  }
+</script>
+
+<h1 class="text-4xl text-white font-bold tracking-wide">{greeting}</h1>
